b5a3b6dd753319eda9f0a37db21ae33e02b1f7d7
Zacaria <zacaria.chtatar.ext@clubmed.com>
2021-12-01 23:55:29 +0100

    [ADD] chall 1

--- src/day1/chall1.rs
+++ src/day1/chall1.rs
@@ -1,4 +1,24 @@
-fn main() {
-    let toto = 456;
-    println!("Hello, world! {}", toto);
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+
+  let file = File::open("src/day1/input1.txt")?;
+  let reader = BufReader::new(file);
+
+  let mut previous_value = 0;
+  let mut counter = 0;
+
+  for (index, line) in reader.lines().enumerate() {
+      let current_value = line?.parse::<i32>().unwrap();
+
+      if index != 0 && previous_value < current_value {
+        counter += 1;
+      }
+      previous_value = current_value;
+  }
+
+  println!("result {}", counter);
+
+  Ok(())
 }
