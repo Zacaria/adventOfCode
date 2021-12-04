c6e2f6eed712f89dd2d6e075c7169000b58739bd
Zacaria <havesomecode@gmail.com>
2021-12-04 14:25:31 +0100

    [UP] Refacto day3 chall 1

--- src/day3/day3chall1.rs
+++ src/day3/day3chall1.rs
@@ -3,6 +3,19 @@ use std::io;
 fn main() -> io::Result<()> {
   let lines = utils::get_lines("src/day3/input.txt");
 
+  let (bits_counter, total_lines) = count_bits(lines);
+
+  let (epsilon, gamma) = get_epsilon_gamma(bits_counter, total_lines);
+
+  let e = utils::to_u32(&epsilon);
+  let g = utils::to_u32(&gamma);
+
+  println!("e {} - g {} - result {}", e, g, e * g);
+
+  Ok(())
+}
+
+fn count_bits(lines: Vec<String>) -> ([i32; 12], i32) {
   let mut bits_counter: [i32; 12] = [0; 12];
   let mut total_lines = 0;
 
@@ -15,22 +28,22 @@ fn main() -> io::Result<()> {
     total_lines += 1;
   }
 
+  (bits_counter, total_lines)
+}
+
+fn get_epsilon_gamma(bits_counter: [i32; 12], total: i32) -> ([u8; 12], [u8; 12]) {
   let mut epsilon: [u8; 12] = [0; 12];
   let mut gamma: [u8; 12] = [0; 12];
   for index in 0..bits_counter.len() {
-    let has_more_one = bits_counter[index] > (total_lines / 2);
+    let has_more_one = bits_counter[index] > (total / 2);
     epsilon[index] = if has_more_one { 1 } else { 0 };
     gamma[index] = if has_more_one { 0 } else { 1 };
   }
 
-  let e = utils::to_u32(&epsilon);
-  let g = utils::to_u32(&gamma);
-
   println!(
     "total {} - counter {:?} - epsilon {:?}  - gamma {:?}",
-    total_lines, bits_counter, epsilon, gamma
+    total, bits_counter, epsilon, gamma
   );
-  println!("e {} - g {} - result {}", e, g, e * g);
 
-  Ok(())
+  (epsilon, gamma)
 }
